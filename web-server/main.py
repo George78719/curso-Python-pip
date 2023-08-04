17d01e6aa2c6fa5c57e25f695f5ee5785312b9b2
Jorge Carrillo <jcbalcazar78@gmail.com>
2023-08-03 20:45:45 -0500

    add modified main - servidor web

--- web-server/main.py
+++ web-server/main.py
@@ -1,4 +1,26 @@
 import store
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+@app.get('/')       # Decorador
+def get_list():
+    return[1, 2, 3]
+
+@app.get('/contact', response_class=HTMLResponse)
+def get_list():
+    return """
+    <html>
+        <head>
+            <title>Hola Soy un Título</title>
+        </head>
+        <body>
+            <h1>Hola Soy una página</h1>
+            <p>soy un parrafo</p>
+        </body>
+    </html>
+    """
 
 def run():
     store.get_categories()
